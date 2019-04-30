7ac63b7805c1eb4cf6e923e3fcee2311a3dc9c95
NDS 강우정 <NDS @LAPTOP-IV1D7MO7>
2019-04-30 14:40:54 +0900

    VO 추가

---
+++ n-wjms/src/main/java/com/nds/nwjms/vo/Emp_infoVO.java
@@ -0,0 +1,54 @@
+package com.nds.nwjms.vo;
+
+public class Emp_infoVO {
+	private String emp_id;
+	private String emp_pw;
+	private String emp_nm;
+	private String sex;
+	private Integer age;
+	private Integer meal_cnt;
+	private String ngtwr_at;
+	
+	public String getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(String emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getEmp_pw() {
+		return emp_pw;
+	}
+	public void setEmp_pw(String emp_pw) {
+		this.emp_pw = emp_pw;
+	}
+	public String getEmp_nm() {
+		return emp_nm;
+	}
+	public void setEmp_nm(String emp_nm) {
+		this.emp_nm = emp_nm;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Integer getMeal_cnt() {
+		return meal_cnt;
+	}
+	public void setMeal_cnt(Integer meal_cnt) {
+		this.meal_cnt = meal_cnt;
+	}
+	public String getNgtwr_at() {
+		return ngtwr_at;
+	}
+	public void setNgtwr_at(String ngtwr_at) {
+		this.ngtwr_at = ngtwr_at;
+	}
+}
---
+++ n-wjms/src/main/java/com/nds/nwjms/vo/Foodticket_dtlsVO.java
@@ -0,0 +1,55 @@
+package com.nds.nwjms.vo;
+
+public class Foodticket_dtlsVO {
+	private String ticket_no;
+	private String emp_id;
+	private String rstrnt_id;
+	private String menu_nm;
+	private String pay_state;
+	private String use_date;
+	private String exp_date;
+	
+	public String getTicket_no() {
+		return ticket_no;
+	}
+	public void setTicket_no(String ticket_no) {
+		this.ticket_no = ticket_no;
+	}
+	public String getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(String emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getRstrnt_id() {
+		return rstrnt_id;
+	}
+	public void setRstrnt_id(String rstrnt_id) {
+		this.rstrnt_id = rstrnt_id;
+	}
+	public String getMenu_nm() {
+		return menu_nm;
+	}
+	public void setMenu_nm(String menu_nm) {
+		this.menu_nm = menu_nm;
+	}
+	public String getPay_state() {
+		return pay_state;
+	}
+	public void setPay_state(String pay_state) {
+		this.pay_state = pay_state;
+	}
+	public String getUse_date() {
+		return use_date;
+	}
+	public void setUse_date(String use_date) {
+		this.use_date = use_date;
+	}
+	public String getExp_date() {
+		return exp_date;
+	}
+	public void setExp_date(String exp_date) {
+		this.exp_date = exp_date;
+	}
+
+}
